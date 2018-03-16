8f1cd7cfd2795d9a3070c356ce9b8539f4c5ab56
jamesfowler <JamesFowlerTX@yahoo.com>
2018-03-16 09:28:10 -0500

    Finished albums mySQLtables exersise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+id INT unsigned NOT NULL AUTO_INCREMENT,
+artist VARCHAR(30) NOT NULL,
+name VARCHAR(100) NOT NULL,
+release_date DATETIME NOT NULL,
+sales FLOAT NOT NULL,
+genre VARCHAR(500) NOT NULL,
+PRIMARY KEY(id)
+);
